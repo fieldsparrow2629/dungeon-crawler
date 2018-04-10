9c719430bc735365174747032901e486c901adf5
Erik Blom <eblom2629@greenvilleschools.us>
2018-04-09 21:36:45 -0400

    added in new choice

--- dungeon.py
+++ dungeon.py
@@ -9,3 +9,12 @@ class Dungeon:
     def room_one(self):
         print("You enter the first room, it is dark and damp.")
         print("You see a fire in the center of the room...")
+        print("Take a torch with you?...")
+        while True:
+            ans = input()
+            if ans == 'y':
+                print("You pick up the torch")
+            if ans == 'n':
+                print("You leave the torch")
+
+            break
